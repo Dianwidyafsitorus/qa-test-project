a1878ce2aa0b798842a30b398288c2ae4b2b9700
Dian Widya Febrianti Sitorus <89889951+Dianwfs1102@users.noreply.github.com>
2025-07-24 19:33:28 +0700

    update yml

--- frontend/cypress.config.js
+++ frontend/cypress.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
+    defaultCommandTimeout: 10000,
     baseUrl: 'http://localhost:5173',
     supportFile: false,
     specPattern: 'tests/e2e/**/*.cy.js',
